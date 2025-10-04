4c0c29b338dab25dd4cec851bb138b28432fd173
Silke pilon <silkepilon2009@gmail.com>
2025-10-04 21:27:00 +0200

    refactor: standardize quotes and formatting in service worker script

--- my-app/public/sw.js
+++ my-app/public/sw.js
@@ -1,14 +1,12 @@
-self.addEventListener('install', (event) => {
-  self.skipWaiting()
-})
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim())
-})
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
 
-self.addEventListener('notificationclick', (event) => {
-  event.notification.close()
-  event.waitUntil(
-    self.clients.openWindow('/')
-  )
-})
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  event.waitUntil(self.clients.openWindow("/"));
+});
